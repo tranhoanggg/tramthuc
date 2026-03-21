db1b69b50a90d862619074fcd663b21a0a611e94
tranhoanggg <tranhoang292004gmail.com>
2026-03-21 17:01:25 +0700

    Fix Cors

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const cors = require("cors");
 const db = require("./src/config/db.js");
 
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-app.use(cors());
 app.use(express.json());
 
 app.get("/api/collections", (req, res) => {
